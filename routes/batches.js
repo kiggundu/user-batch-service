f04dbd8f1d892005d398df4a3f542a6155449dde
Abraham Kiggundu <abraham@thekiggys.com>
2018-06-05 15:33:36 +0300

    End to end with the service

--- routes/batches.js
+++ routes/batches.js
@@ -1,9 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var fetch = require('fetch');
+var request = require('request');
 
-/* GET users listing. */
+/* Post batches. */
 router.post('/', function(req, res, next) {
+    try{
+        // console.log(req.body);
+        // console.log("req.body========================");
+        const baseUrl = req.body.endpoint.url;
+        const userBatchUpdates = req.body.payload;
+        
+        userBatchUpdates.map((user) => {
+            request.post(
+                baseUrl + "/" + user.path,
+                user.body,
+                function (error, response, body) {
+                    if (!error && response.statusCode == 200) {
+                        console.log(body)
+                    }
+                    console.log(response.statusCode)
+                }
+            );
+        });
+
+    }
+    catch(error){
+        console.log(error);
+    }
+
+
   res.send('Batch endpoint is live');
 });
 
